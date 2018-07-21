80e0385f9f500767e77e2a6d76967bafbb773963
Alexander Do <alexanderhdo@gmail.com>
2018-07-20 17:11:56 -0700

    modify

--- translate.py
+++ translate.py
@@ -1,2 +1,10 @@
 phrase = input("Enter some text: ")
-leet_phrase = phrase.replace()
+phrase = phrase.replace("a", "4")
+phrase = phrase.replace("b", "8")
+phrase = phrase.replace("e", "3")
+phrase = phrase.replace("l", "1")
+phrase = phrase.replace("o", "0")
+phrase = phrase.replace("s", "5")
+phrase = phrase.replace("t", "7")
+
+print(phrase)
